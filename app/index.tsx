4198b479f1f7d7e94ba5b774837fa9f103ae195d
mary <mariafernandabernalvaldez@gmail.com>
2026-03-03 15:41:59 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
-
 export default function Index() {
+  useEffect(() => {
+    console.log("entre en pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const url = "https://pokeapi.co/api/v2/pokemon/1/";
+    const response = await fetch(url); /*request a la api */
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Yhael</Text>
     </View>
   );
 }
